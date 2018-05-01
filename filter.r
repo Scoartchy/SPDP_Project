e2665df94e11b854b67c0de9bc8a77e16afdf2e4
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2018-05-01 19:54:12 +0200

    geodata filtering

--- filter.r
+++ filter.r
@@ -10,3 +10,7 @@ filtered_logs <- filtered_logs[!is.na(filtered_logs$CLIWOC21.Lat3), ]
 filtered_logs <- filtered_logs[!is.na(filtered_logs$CLIWOC21.Lon3), ]
 filtered_logs <- filtered_logs[, c("CLIWOC21.RecID", "CLIWOC21.VoyageFrom", "CLIWOC21.VoyageTo", "CLIWOC21.Nationality", "CLIWOC21.Lat3", "CLIWOC21.Lon3", 
                                    "CLIWOC21.Anchored", "CLIWOC21.WindDirection", "CLIWOC21.WindForce")]
+geodata <- sources[[6]]
+filtered_geodata <- geodata[!is.na(geodata$Geodata.DecLongitude), ]
+filtered_geodata <- filtered_geodata[!is.na(filtered_geodata$Geodata.DecLatitude), ]
+filtered_geodata <- filtered_geodata[, c("Geodata.ID", "Geodata.DecLatitude", "Geodata.DecLongitude")]
