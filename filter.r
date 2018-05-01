f1775ddf56730761971d32105dd9854f8fe14087
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2018-05-01 19:46:06 +0200

    added wind direction

--- filter.r
+++ filter.r
@@ -9,4 +9,4 @@ filtered_logs <- filtered_logs[filtered_logs$CLIWOC21.Year <= 1850, ]
 filtered_logs <- filtered_logs[!is.na(filtered_logs$CLIWOC21.Lat3), ]
 filtered_logs <- filtered_logs[!is.na(filtered_logs$CLIWOC21.Lon3), ]
 filtered_logs <- filtered_logs[, c("CLIWOC21.RecID", "CLIWOC21.VoyageFrom", "CLIWOC21.VoyageTo", "CLIWOC21.Nationality", "CLIWOC21.Lat3", "CLIWOC21.Lon3", 
-                                   "CLIWOC21.Anchored", "CLIWOC21.WindForce")]
+                                   "CLIWOC21.Anchored", "CLIWOC21.WindDirection", "CLIWOC21.WindForce")]
