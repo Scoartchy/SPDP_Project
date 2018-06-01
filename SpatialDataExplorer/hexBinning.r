cb892207f63eaaa61bcfcf68b709d53909f7267c
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2018-06-01 15:49:21 +0200

    added hexbinning for ships and improved visuals

--- SpatialDataExplorer/hexBinning.r
+++ SpatialDataExplorer/hexBinning.r
@@ -1,18 +1,84 @@
 #hexBinning
 install.packages("fMultivar")
-longitude <- file2[["Geodata.DecLongitude"]]
-latitude <- file2[["Geodata.DecLatitude"]]
-hbin <- fMultivar::hexBinning(longitude, latitude)
-plot(hbin)
+library(ggplot2)
+library(fMultivar)
+longitude <- geodata$Geodata.DecLongitude
+latitude <- geodata$Geodata.DecLatitude
 
-plotted <- filtered_logs[1:1000,]
-atlantic <- ggplot2::map_data('world')
-ggplot() + geom_polygon(data = atlantic, aes(x=long, y = lat, group = group)) + 
-  ggplot2::geom_point(mapping = aes(plotted$CLIWOC21.Lon3, plotted$CLIWOC21.Lat3), size=0) +
-  ggplot2::geom_hex(aes(hbin$x, hbin$y, fill=hbin$z), color="black", stat="identity")
-  coord_fixed()
+################################ For geodata ie ports ####################################################
 
-longitude <- file1[["CLIWOC21.Lon3"]]
-latitude <- file1[["CLIWOC21.Lat3"]]
-hbin <- fMultivar::hexBinning(longitude, latitude) #Error in if (dist1 < con1) { : missing value where TRUE/FALSE needed
-plot(hbin)
+#using fmultivar for binning
+hbin <- hexBinning(longitude, latitude)
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(hbin$x, hbin$y, fill=hbin$z), color="black", stat="identity", alpha=0.8) +
+  ggtitle("fMultivar binning with 30 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+  
+#lets see with more bins
+hbin <- hexBinning(longitude, latitude, bins=60)
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(hbin$x, hbin$y, fill=hbin$z), color="black", stat="identity", alpha=0.8) +
+  ggtitle("fMultivar binning with 60 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+  
+#ggplot has its own binning too!
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+    geom_hex(aes(x = longitude, y = latitude), color="black", alpha=0.8) +
+    ggtitle("ggplot binning with 30 bins") +
+    labs(fill="count") +
+    coord_fixed(1.3)
+
+#and with more bins
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(x = longitude, y = latitude), color="black", alpha=0.8, bins=60) +
+  ggtitle("ggplot binning with 60 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+
+############################################## For ships ########################################################
+
+#################For all 
+
+longitude <- filtered_logs$CLIWOC21.Lon3
+latitude <- filtered_logs$CLIWOC21.Lat3
+
+#using fmultivar for binning
+hbin <- hexBinning(longitude, latitude)
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(hbin$x, hbin$y, fill=hbin$z), color="black", stat="identity", alpha=0.8) +
+  ggtitle("fMultivar binning with 30 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+
+#lets see with more bins
+hbin <- hexBinning(longitude, latitude, bins=60)
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(hbin$x, hbin$y, fill=hbin$z), color="black", stat="identity", alpha=0.8) +
+  ggtitle("fMultivar binning with 60 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+
+#ggplot has its own binning too!
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(x = longitude, y = latitude), color="black", alpha=0.8) +
+  ggtitle("ggplot binning with 30 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+
+#and with more bins
+wmap <- map_data('world')
+ggplot() + geom_polygon(data = wmap, aes(x=long, y = lat, group = group)) + 
+  geom_hex(aes(x = longitude, y = latitude), color="black", alpha=0.8, bins=45) +
+  ggtitle("ggplot binning with 60 bins") +
+  labs(fill="count") +
+  coord_fixed(1.3)
+
+#### Looks a little messy doesn't it? Perhaps let's try a more robust analysis
+
