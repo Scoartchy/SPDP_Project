c92a1b5796533b5126c29cce777e5a4812bb1e35
Scoartchy <scoarthy@gmail.com>
2018-06-06 18:06:45 +0200

    .
    Small changes in plotPattern, added images

--- SpatialDataExplorer/plotPattern.r
+++ SpatialDataExplorer/plotPattern.r
@@ -11,11 +11,11 @@ latitude <- filtered_geodata$Geodata.DecLatitude
 planarPointPattern <- ppp(longitude, latitude, c(-180, 180), c(-90,90))
 summary(planarPointPattern)
 
-plot(planarPointPattern, "Wzorzec punkt?w dla port?w")
 planarPointPattern = as.data.frame(planarPointPattern)
 plot <- ggplot() +
-  geom_point(aes(x=planarPointPattern$x, y = planarPointPattern$y)) +
-  ggtitle("Point pattern for ports") 
+  geom_point(aes(x=planarPointPattern$x, y = planarPointPattern$y), size = 0) +
+  ggtitle("Point pattern for ports") +
+  labs(x = "Longitude", y = "Latitude")
 ggsave(filename = paste("out/pointPatterns/point_pattern_ports.png"), plot)
 
 
@@ -25,9 +25,11 @@ latitude <- filtered_logs$CLIWOC21.Lat3
 planarPointPattern <- ppp(longitude, latitude, c(-180, 180), c(-90,90))
 summary(planarPointPattern)
 
-plot(planarPointPattern, "Wzorzec punkt?w dla miejsc odwiedzonych w trakcie podr??y")
-
-plot <- ggplot(data=planarPointPattern, aes(x=x, y=y, group=1)) +
-  geom_point() +
-  ggtitle("Point pattern for travel points") 
+planarPointPattern = as.data.frame(planarPointPattern)
+plot <- ggplot() +
+  geom_point(aes(x=planarPointPattern$x, y = planarPointPattern$y), size = 0) +
+  ggtitle("Point pattern for travel points") +
+  labs(x = "Longitude", y = "Latitude")
 ggsave(filename = paste("out/pointPatterns/point_pattern_voyage.png"), plot)
+
+
